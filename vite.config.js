2a3df598708b88ef07b46eec70e9a0826cfb0f2a
jgq-cell <17318646019@163.com>
2023-05-28 10:24:33 +0800

    feat: 定制elementPlus主题颜色-color色彩

--- vite.config.js
+++ vite.config.js
@@ -14,13 +14,26 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()]
+      resolvers: [
+        // 1、配置elementPlus采用scss样式配色系统
+        ElementPlusResolver({ importStyle: 'sass' })
+      ]
     })
   ],
   resolve: {
     // 实际的路径转换`@-> ./src`
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2、自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `
+      }
     }
   }
 })
