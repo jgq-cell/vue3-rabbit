941e7bd2cc7bdc3dddba12642335c43634317050
jgq-cell <17318646019@163.com>
2023-05-28 21:42:16 +0800

    feat: 路由配置、静态资源引入

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import './assets/main.css'
+// 引入初始化样式
+import '@/styles/common.scss'
 
 // 测试接口函数
 import { getCategory } from './apis/testAPI'
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// createRouter: 创建router实例对象
+// createWebHistory： 创建history模式的路由
+
+// vite引擎需要写到index.vue，否则无法识别
+import Login from '@/views/Login/index.vue'
+import Layout from '@/views/Layout/index.vue'
+import Home from '@/views/Home/index.vue'
+import Category from '@/views/Category/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // routes: 配置path和component对应关系的位置
   routes: [
+    // 一级路由
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: Layout,
+      children: [
+        {
+          path: '', // 默认二级路由：当访问父路由时，会默认访问子组件
+          component: Home
+        },
+        {
+          path: 'category',
+          component: Category
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      component: Login
     }
   ]
 })
