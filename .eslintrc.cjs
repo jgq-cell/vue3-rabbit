d2ab2314ae97bebe1bf40186d460c6646e2b24c1
jgq-cell <17318646019@163.com>
2023-05-28 09:45:17 +0800

    feat: 别名路径联想提示、代码格式化配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,12 @@
 /* eslint-env node */
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended"
-  ]
+  root: true,
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+  rules: {
+    // 解决prettier与eslint规则冲突
+    indent: 0,
+    'space-before-function-paren': 0,
+    // 关闭组件命名规则
+    'vue/multi-word-component-names': 'off'
+  }
 }
