48fa76b4fac2860d2d7230362703b12df81ea8af
jgq-cell <17318646019@163.com>
2023-07-17 22:54:15 +0800

    feat: 详情-整体业务认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,12 +24,16 @@ const router = createRouter({
           component: Home
         },
         {
-          path: '/category/:id',
+          path: 'category/:id',
           component: Category
         },
         {
-          path: '/category/sub/:id',
+          path: 'category/sub/:id',
           component: SubCategory
+        },
+        {
+          path: 'detail/:id',
+          component: Detail
         }
       ]
     },
