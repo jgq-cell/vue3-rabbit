05df86eb083443a3420ebde265a2966ebebb9e1c
jgq-cell <17318646019@163.com>
2023-07-23 15:00:45 +0800

    feat: 图片预览组件-小图切换大图

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/ImageView/index.vue'
 import { getDetail } from '@/apis/detail'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
@@ -42,7 +43,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
