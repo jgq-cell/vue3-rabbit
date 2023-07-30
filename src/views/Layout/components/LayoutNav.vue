d593c34742991316e4441cde1e729e7380907c3b
jgq-cell <17318646019@163.com>
2023-07-30 12:08:52 +0800

    feat: 登录-使用token适配多模板

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,13 +1,20 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from '@/stores/user'
+const userStore = useUserStore()
+</script>
 
 <template>
   <nav class="app-topnav">
     <div class="container">
       <ul>
         <!-- 多模板渲染：用于区分登录状态和非登录状态 -->
-        <template v-if="false">
+        <!-- 思路：登录时显示第一块，非登录时显示第二块，取决于是否有token-->
+        <template v-if="userStore.userInfo.token">
           <li>
-            <a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a>
+            <a href="javascript:;"
+              ><i class="iconfont icon-user"></i
+              >{{ userStore.userInfo.account }}</a
+            >
           </li>
           <li>
             <el-popconfirm
