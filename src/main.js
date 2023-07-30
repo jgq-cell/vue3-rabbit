5a2836f6a72fa20125246c4efa72fab2ca248419
jgq-cell <17318646019@163.com>
2023-07-30 11:57:28 +0800

    feat: 登录-pinia用户数据持久化

--- src/main.js
+++ src/main.js
@@ -2,16 +2,21 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 // 引入初始化样式
 import '@/styles/common.scss'
 // 引入懒加载指令插件并注册
 import { lazyPlugin } from './directives'
 // 引入全局组件并注册
 import { componentPlugin } from '@/components'
-const app = createApp(App)
 
-app.use(createPinia())
+const app = createApp(App)
+const pinia = createPinia()
+// 注册持久化插件
+app.use(pinia)
+pinia.use(piniaPluginPersistedstate)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
+
 app.mount('#app')
--- src/stores/user.js
+++ src/stores/user.js
@@ -2,19 +2,26 @@ import { defineStore } from 'pinia'
 import { loginAPI } from '@/apis/user'
 import { ref } from 'vue'
 
-export const useUserStore = defineStore('user', () => {
-  // 管理用户数据管理
-  const userInfo = ref([])
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    // 管理用户数据管理
+    // token 用来标识当前用户是否登录，可设置过期时间
+    // pinia存储基于内存，刷新会丢失，为了保持登录状态需做持久化存储
+    const userInfo = ref([])
 
-  // action 获取导航数据的方法
-  const getUserInfo = async ({ account, password }) => {
-    const res = await loginAPI({ account, password })
-    userInfo.value = res.result
-  }
+    // action 获取导航数据的方法
+    const getUserInfo = async ({ account, password }) => {
+      const res = await loginAPI({ account, password })
+      userInfo.value = res.result
+    }
 
-  // 暴露出管理的数据和方法
-  return {
-    userInfo,
-    getUserInfo
-  }
-})
+    // 暴露出管理的数据和方法
+    return {
+      userInfo,
+      getUserInfo
+    }
+  },
+  // 用户数据持久化：设置state的同时，会同步在LocalStorege
+  { persist: true }
+)
