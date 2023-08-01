acce84ed30848424cc50290501bc13b3f37aaf03
jgq-cell <17318646019@163.com>
2023-08-01 21:03:17 +0800

    feat: 本地购物车-结算统计

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,7 +1,7 @@
 // 封装购物车模块
 
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 export const useCartStore = defineStore(
   'cart',
@@ -13,8 +13,8 @@ export const useCartStore = defineStore(
       // 添加购物车操作
       const item = cartList.value.find((item) => goods.skuId === item.skuId)
       if (item) {
-        // 已添加过 - count + 1
-        item.count++
+        // 已添加过 - 原来购物车数量+添加的数量
+        item.count += goods.count
       } else {
         // 没有添加过 - 直接push
         cartList.value.push(goods)
@@ -28,10 +28,23 @@ export const useCartStore = defineStore(
         cartList.value.splice(idx, 1)
       }
     }
+    // 3、定义computed - 依赖属性变化会立刻计算
+    // 总数量=所有商品的count之和
+    // reduce函数：第一个参数a 初始化为0， 第二个参数c 是数组当前值，每次运算后将结果赋值给a
+    const allCount = computed(() =>
+      cartList.value.reduce((a, c) => a + c.count, 0)
+    )
+    // 总价=所有商品的count*price之和
+    const allPrice = computed(() =>
+      cartList.value.reduce((a, c) => a + c.count * c.price, 0)
+    )
+
     return {
       cartList,
       addCart,
-      delCart
+      delCart,
+      allCount,
+      allPrice
     }
   },
   { persist: true }
