b0e20d2bed1d2ec8b9df61466bda3ac018be6f76
jgq-cell <17318646019@163.com>
2023-08-01 22:28:54 +0800

    feat: 本地购物车-购物车列表全选

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -45,13 +45,20 @@ export const useCartStore = defineStore(
       const item = cartList.value.find((item) => item.skuId == skuId)
       item.selected = selected
     }
+    // 6、定义action - 全选功能
+    const isAll = computed(() => cartList.value.every((item) => item.selected))
+    const allCheck = (selected) => {
+      cartList.value.forEach((item) => (item.selected = selected))
+    }
     return {
       cartList,
       addCart,
       delCart,
       allCount,
       allPrice,
-      singleCheck
+      singleCheck,
+      isAll,
+      allCheck
     }
   },
   { persist: true }
